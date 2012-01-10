4418bba628cc51b59c63ce0717af550ff1829abb
sebcsaba <sebcsaba@freemail.hu>
2012-01-10 20:23:14 +0100

    test added

---
+++ src/test/hu/sebcsaba/georesults/JenkinsTest.java
@@ -0,0 +1,12 @@
+package hu.sebcsaba.georesults;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class JenkinsTest {
+
+	@Test
+	public void foo() {
+		assertTrue(false);
+	}
+	
+}
