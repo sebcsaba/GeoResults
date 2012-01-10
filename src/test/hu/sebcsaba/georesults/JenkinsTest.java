55bfc66d9590a36532dc6408b46ed6e3fd9f1715
sebcsaba <sebcsaba@freemail.hu>
2012-01-10 21:03:44 +0100

    improve test

--- src/test/hu/sebcsaba/georesults/JenkinsTest.java
+++ src/test/hu/sebcsaba/georesults/JenkinsTest.java
@@ -2,11 +2,12 @@ package hu.sebcsaba.georesults;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+
 public class JenkinsTest {
 
 	@Test
 	public void foo() {
-		assertTrue(false);
+		assertTrue(true);
 	}
 	
 }
